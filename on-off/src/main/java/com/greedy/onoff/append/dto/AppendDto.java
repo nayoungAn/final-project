0b10824962e2386f417e269514a37f69d351c578
yunji86 <rhaehfdlvn91@gmail.com>
2022-11-17 17:04:11 +0900

    [김윤지] appendDto 수정

--- on-off/src/main/java/com/greedy/onoff/append/dto/AppendDto.java
+++ on-off/src/main/java/com/greedy/onoff/append/dto/AppendDto.java
@@ -5,7 +5,7 @@ import com.greedy.onoff.member.dto.MemberDto;
 import lombok.Data;
 
 @Data
-public class AppendDTO {
+public class AppendDto {
 	
 	private Long appendCode;
 	private String appendFile;
