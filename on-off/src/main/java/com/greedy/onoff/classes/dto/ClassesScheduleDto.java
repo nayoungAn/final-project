a2cfd4174a9f5af668544e49180b84f718e3f434
yunji86 <rhaehfdlvn91@gmail.com>
2022-11-17 18:08:29 +0900

    [김윤지] 회원 디티오,엔티티 수정

---
+++ on-off/src/main/java/com/greedy/onoff/classes/dto/ClassesScheduleDto.java
@@ -0,0 +1,13 @@
+package com.greedy.onoff.classes.dto;
+
+
+import lombok.Data;
+
+@Data
+public class ClassesScheduleDto {
+	
+	  private ClassesDto classes;
+	  private DayDto day;
+	  private TimeDto time;
+	
+}
---
+++ on-off/src/main/java/com/greedy/onoff/mtm/dto/mtmDto.java
@@ -0,0 +1,17 @@
+package com.greedy.onoff.mtm.dto;
+
+import java.sql.Date;
+
+public class mtmDto {
+	
+	 	private Long mtmCode;
+	    private Date mtmDate;
+	    private String mtmTitle;
+	    private String mtmDescription;
+	    private MemberDto member;
+	    private ClassesDto classes;
+	    private Long mtmrefer;
+	    private Long answerCode;
+	    private String mtmdelete;
+
+}
