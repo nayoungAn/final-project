2f6e40cacd7bf2aacc26a1c5b6d2c0eb3d6d7e48
yunji86 <rhaehfdlvn91@gmail.com>
2022-11-21 11:10:58 +0900

    [김윤지] 로그인

--- on-off/src/main/java/com/greedy/onoff/acc/repository/AccRepository.java
+++ on-off/src/main/java/com/greedy/onoff/acc/repository/AccRepository.java
@@ -4,10 +4,12 @@ package com.greedy.onoff.acc.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.greedy.onoff.acc.entity.Acc;
+import com.greedy.onoff.member.entity.Member;
 
-public interface AccRepository{
+public interface AccRepository  extends JpaRepository<Acc, Long>{
 
 	/* 수납 내역 조회 */
 	@EntityGraph(attributePaths = {"member"})
