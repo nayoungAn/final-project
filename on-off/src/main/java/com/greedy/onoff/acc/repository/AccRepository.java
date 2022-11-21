2d3e97062a1a80cbde4bbf68b927043e308c86b0
sme1029 <seong161718@gmail.com>
2022-11-21 09:44:52 +0900

    [성현기] 수납 관리 조회 작업 중

---
+++ on-off/src/main/java/com/greedy/onoff/acc/repository/AccRepository.java
@@ -0,0 +1,16 @@
+package com.greedy.onoff.acc.repository;
+
+import java.awt.print.Pageable;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
+
+import com.greedy.onoff.acc.entity.Acc;
+
+public interface AccRepository {
+
+	/* 수납 내역 조회 */
+	@EntityGraph(attributePaths = {"member"})
+	Page<Acc> findAll(org.springframework.data.domain.Pageable pageable);
+
+}
