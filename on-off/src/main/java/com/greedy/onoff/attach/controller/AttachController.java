52bcd51112564f01593d6d2748cc823a1e038d24
An nayoung <any92nabee@gmail.com>
2022-11-22 12:23:39 +0900

    [안나영]부분수정

--- on-off/src/main/java/com/greedy/onoff/attach/controller/AttachController.java
+++ on-off/src/main/java/com/greedy/onoff/attach/controller/AttachController.java
@@ -1,28 +1,30 @@
-package com.greedy.onoff.attach.controller;
+//package com.greedy.onoff.attach.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.greedy.onoff.attach.service.AttachService;
+//import com.greedy.onoff.classes.dto.OpenClassesDto;
+//
+//import lombok.extern.java.Log;
+//
+//@RestController
+//@RequestMapping("/ono")
+//public class AttachController {
+//	
+//	private final AttachService attachService;
+//	
+//	public AttachController(AttachService attachService) {
+//		this.attachService = attachService;
+//	}
+//}
+//	
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.greedy.onoff.attach.service.AttachService;
-import com.greedy.onoff.classes.dto.OpenClassesDto;
-
-import lombok.extern.java.Log;
-
-@RestController
-@RequestMapping("/ono")
-public class AttachController {
-	
-	private final AttachService attachService;
-	
-	public AttachController(AttachService attachService) {
-		this.attachService = attachService;
-	}
-}
-	
+		/*1. 내강의 목록조회(강사) - 페이징 , 로그인한 강사의 해당수업 강의만 조회  */
 //	
 //	@GetMapping("/ono/myclass")
 //	public List<OpenClassesDto> myclass(@RequestParam(name="page", defaultValue="1")int page) {
