a39077e672b8d09fa0eabd030683528b91a24937
kimdonghyun <donghyun9191@gmail.com>
2022-11-24 09:50:10 +0900

    [김동현] 등록상담 조회 url 수정

--- on-off/src/main/java/com/greedy/onoff/cons/controller/ConsController.java
+++ on-off/src/main/java/com/greedy/onoff/cons/controller/ConsController.java
@@ -45,7 +45,7 @@ private final ConsService consService;
 	}
 	
 	/* 2. 등록 상담 목록 조회 - 페이징, 주문 불가 상품 포함 (관리자) */
-	@GetMapping("/cons-management")
+	@GetMapping("/cons")
 	public ResponseEntity<ResponseDto> selectProductListForAdmin(@RequestParam(name="page", defaultValue="1") int page) {
 		
 		log.info("[ProductController] selectProductListForAdmin Start ================================");
