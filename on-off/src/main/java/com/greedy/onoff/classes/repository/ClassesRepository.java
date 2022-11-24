54069d22b39049e414a2a32811eebf9d5dbea348
dongyoung Kim <passby_@naver.com>
2022-11-24 17:54:33 +0900

    [김동영] 강사 이력 crud

---
+++ on-off/src/main/java/com/greedy/onoff/classes/repository/ClassesRepository.java
@@ -0,0 +1,31 @@
+package com.greedy.onoff.classes.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.greedy.onoff.classes.entity.OpenClasses;
+
+
+public interface ClassesRepository extends JpaRepository<OpenClasses, Long> {
+
+
+	/* 강의 목록 조회 - 페이징, 상태 여부 'N' 포함 (관리자) */
+	@EntityGraph(attributePaths= {"member"})
+	Page<OpenClasses> findAll(Pageable pageable);
+
+	/* 강의 목록 조회 - 강의명 검색 기준, 페이징, 상태 여부 'N' 포함 (관리자)  */
+	Page<OpenClasses> findByClassNameContains(Pageable pageable, String className);
+	
+	/* 강의 상세 조회 - classCode로 강의 1개 조회, 상태 여부 'N' 포함 (관리자) */
+	Optional<OpenClasses> findByClassCode(Long classCode) ;
+	
+
+
+	
+}
