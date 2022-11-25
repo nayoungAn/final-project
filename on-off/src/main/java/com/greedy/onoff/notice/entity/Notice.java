b218c4e4f035fe184c1107cc142a3d87587f5660
SJcodin <soslzk7653@gmail.com>
2022-11-25 09:43:17 +0900

    [이성준] 공지사항 엔티티수정

--- on-off/src/main/java/com/greedy/onoff/notice/entity/Notice.java
+++ on-off/src/main/java/com/greedy/onoff/notice/entity/Notice.java
@@ -1,12 +1,12 @@
 package com.greedy.onoff.notice.entity;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -38,7 +38,8 @@ public class Notice {
 	@Column(name = "NOTICE_DATE")
 	private String noticeDate;
 	
-	@Column(name = "MEMBER_CODE")
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_CODE")
 	private Long member;
 	
 	
