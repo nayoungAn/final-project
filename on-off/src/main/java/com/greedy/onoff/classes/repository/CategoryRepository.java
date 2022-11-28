338f61f70420fb7c57078621799f299e69bf5f94
nayoungAn <114467524+nayoungAn@users.noreply.github.com>
2022-11-28 16:11:32 +0900

    Delete CategoryRepository.java

--- on-off/src/main/java/com/greedy/onoff/classes/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.greedy.template.product.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import com.greedy.template.product.entity.Category;
-//
-//public interface CategoryRepository extends JpaRepository<Category, Long> {
-//
-//}
