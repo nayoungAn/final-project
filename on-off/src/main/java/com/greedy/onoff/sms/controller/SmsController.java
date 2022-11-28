7de38553a4824d5623ad808616a74a9443990f38
sme1029 <seong161718@gmail.com>
2022-11-28 17:07:24 +0900

    [성현기] sms 보완

--- on-off/src/main/java/com/greedy/onoff/sms/controller/SmsController.java
+++ on-off/src/main/java/com/greedy/onoff/sms/controller/SmsController.java
@@ -1,36 +1,42 @@
-//package com.greedy.onoff.sms.controller;
-//
-//import java.util.List;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.greedy.onoff.classes.entity.ClassesHistory;
-//import com.greedy.onoff.common.ResponseDto;
-//import com.greedy.onoff.sms.dto.SmsCriteria;
-//import com.greedy.onoff.sms.service.SmsService;
-//
-//@RestController
-//@RequestMapping("/ono/sms")
-//public class SmsController {
-//	
-//	private final SmsService smsService;
-//	
-//	public SmsController(SmsService smsService) {
-//		this.smsService = smsService;
-//	}
-//	
-//	/* 문자 전송 대상 조회 */
-//	@GetMapping("")
-//	public ResponseEntity<ResponseDto> selectSmsListForAdmin(@RequestBody SmsCriteria smsCriteria){
-//		
-//		List<ClassesHistory> classesHistory = smsService.selectSmsListForAdmin(smsCriteria);
-//		
-//		return null;
-//		
-//	}
-//
-//}
+package com.greedy.onoff.sms.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.greedy.onoff.common.ResponseDto;
+import com.greedy.onoff.common.paging.Pagenation;
+import com.greedy.onoff.common.paging.PagingButtonInfo;
+import com.greedy.onoff.common.paging.ResponseDtoWithPaging;
+import com.greedy.onoff.sms.dto.SmsDto;
+import com.greedy.onoff.sms.service.SmsService;
+
+@RestController
+@RequestMapping("/ono/sms")
+public class SmsController {
+	
+	private final SmsService smsService;
+	
+	public SmsController(SmsService smsService) {
+		this.smsService = smsService;
+	}
+	
+	/* 문자 전송 대상 조회 */
+	@GetMapping("/sms-management")
+	public ResponseEntity<ResponseDto> selectSmsListForAdmin(@RequestParam(name="page", defaultValue = "1") int page){
+		
+		Page<SmsDto> smsDtoList = smsService.selectSmsListForAdmin(page);
+		PagingButtonInfo pageInfo = Pagenation.getPagingButtonInfo(smsDtoList);
+		
+		ResponseDtoWithPaging responseDtoWithPaging = new ResponseDtoWithPaging();
+		responseDtoWithPaging.setPageInfo(pageInfo);
+		responseDtoWithPaging.setData(smsDtoList.getContent());
+		
+		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "문자 전송 대상 조회 성공!", responseDtoWithPaging));
+	}
+
+}
