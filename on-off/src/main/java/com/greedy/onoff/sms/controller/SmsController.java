b9b10822106ecf1de7c5189db0a2c7cf8eb4c44b
sme1029 <seong161718@gmail.com>
2022-12-01 23:11:52 +0900

    [성현기] sms 조회 완료

--- on-off/src/main/java/com/greedy/onoff/sms/controller/SmsController.java
+++ on-off/src/main/java/com/greedy/onoff/sms/controller/SmsController.java
@@ -5,13 +5,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.greedy.onoff.classes.dto.ClassesHistoryDto;
 import com.greedy.onoff.common.ResponseDto;
-import com.greedy.onoff.sms.dto.SmsCriteria;
 import com.greedy.onoff.sms.service.SmsService;
 
 @RestController
@@ -26,9 +25,9 @@ public class SmsController {
 	
 	/* 문자 전송 대상 조회 */
 	@GetMapping("/sms")
-	public ResponseEntity<ResponseDto> selectSmsListForAdmin(@RequestBody SmsCriteria smsCriteria){
+	public ResponseEntity<ResponseDto> selectSmsListForAdmin(@RequestParam String search ){
 		
-		List<ClassesHistoryDto> classesHistory = smsService.selectSmsListForAdmin(smsCriteria);
+		List<ClassesHistoryDto> classesHistory = smsService.selectSmsListForAdmin(search);
 		
 		return ResponseEntity.ok().body(new ResponseDto(HttpStatus.OK, "문자 전송 대상 조회 완료!", classesHistory)) ;
 		
