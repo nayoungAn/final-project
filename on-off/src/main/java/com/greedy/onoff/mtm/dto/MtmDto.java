281f291da8895cb74b07dfd0b0aaf9449e268db5
sme1029 <seong161718@gmail.com>
2022-12-01 23:19:14 +0900

    [성현기] mtm 오류로 주석 처리함

--- on-off/src/main/java/com/greedy/onoff/mtm/dto/MtmDto.java
+++ on-off/src/main/java/com/greedy/onoff/mtm/dto/MtmDto.java
@@ -1,24 +1,24 @@
-package com.greedy.onoff.mtm.dto;
-
-
-import java.util.Date;
-import com.greedy.onoff.classes.dto.OpenClassesDto;
-import com.greedy.onoff.member.dto.MemberDto;
-import com.greedy.onoff.re.dto.ReDto;
-
-import lombok.Data;
-
-@Data
-public class MtmDto {
-	
-	 	private Long mtmCode;
-	    private Date mtmDate;
-	    private String mtmTitle;
-	    private String mtmDescription;
-	    private MemberDto member;
-	    private OpenClassesDto classes;
-	    private Long answerCode;
-	    private String mtmDelete;
-	    private ReDto reList;
-
-}
+//package com.greedy.onoff.mtm.dto;
+//
+//
+//import java.util.Date;
+//import com.greedy.onoff.classes.dto.OpenClassesDto;
+//import com.greedy.onoff.member.dto.MemberDto;
+//import com.greedy.onoff.re.dto.ReDto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class MtmDto {
+//	
+//	 	private Long mtmCode;
+//	    private Date mtmDate;
+//	    private String mtmTitle;
+//	    private String mtmDescription;
+//	    private MemberDto member;
+//	    private OpenClassesDto classes;
+//	    private Long answerCode;
+//	    private String mtmDelete;
+//	    private ReDto reList;
+//
+//}
