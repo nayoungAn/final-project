dea086f731c64faacea087b7f959a4db700dd8ef
nayoungAn <114467524+nayoungAn@users.noreply.github.com>
2022-12-05 10:56:08 +0900

    [안나영] 코드 수정

--- on-off/src/main/java/com/greedy/onoff/attach/repository/StudentInfoRepository.java
+++ on-off/src/main/java/com/greedy/onoff/attach/repository/StudentInfoRepository.java
@@ -16,7 +16,7 @@ public interface StudentInfoRepository extends JpaRepository<Member, Long> {
 	/*7. 학생정보 조회*/
 	
 	@Query("SELECT m from Member m WHERE m.memberRole='ROLE_STUDENT'")
-	Page<Member> findByMemberNameContains(Pageable pageable, String memberName);
+	Page<Member> findByMemberNameContains(Pageable pageable, String memberCode);
 	
 
 	
